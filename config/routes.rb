16c202aac84f68379d2de9031687ce549d2cc34a
paudelprerana <paudelprerana015@gmail.com>
2016-05-04 16:48:19 +0545

    chapter 2 completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 MarketPlaceApi::Application.routes.draw do
-  #API Definition
-  namespace  :api defaults:  { format: :json }, constraints: { subdomain: 'api' }, path: '/' do
-  
+  # Api definition
+  namespace :api, defaults: { format: :json },
+                              constraints: { subdomain: 'api' }, path: '/'  do
+   
+   scope module: :v1 do
+
+   end # We are going to list our resources here
   end
 end
